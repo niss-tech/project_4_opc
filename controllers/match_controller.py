abfa256db387221922cebee29b6f70894a396f79
nisrine adamo <nisrineadamo1@gmail.com>
2025-01-24 17:41:47 +0100

    update_match_controller

--- controllers/match_controller.py
+++ controllers/match_controller.py
@@ -3,8 +3,6 @@ from models.match import Match
 class MatchController:
     def __init__(self):
         self.matches = []  # Garde une trace de tous les matchs
-
-
    
 
     def list_matches(self):
@@ -20,17 +18,3 @@ class MatchController:
                 )
 
 
-# def create_match(self, player_1, player_2):
-    #     """Permet de créer un match entre deux joueurs et de l'ajouter à la liste des matchs."""
-    #     if not player_1 or not player_2:
-    #         print("Both players are required to create a match.")
-    #         return None
-    #     if player_1 == player_2:
-    #         print("A player cannot play against themselves.")
-    #         return None
-        
-    #     # Création du match sans score initial, car l'utilisateur le saisira plus tard
-    #     new_match = Match(player_1, player_2)
-    #     self.matches.append(new_match)
-    #     print(f"Match created between {player_1.first_name} and {player_2.first_name}.")
-    #     return new_match
