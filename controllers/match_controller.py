433120394f3101ec3b736459f2612e035c4506d4
nisrine adamo <nisrineadamo1@gmail.com>
2025-02-03 03:43:06 +0100

    correctionflake8 de match_controller

--- controllers/match_controller.py
+++ controllers/match_controller.py
@@ -1,9 +1,6 @@
-from models.match import Match
-
 class MatchController:
     def __init__(self):
         self.matches = []  # Garde une trace de tous les matchs
-   
 
     def list_matches(self):
         """Liste tous les matchs créés, avec les scores enregistrés."""
@@ -13,8 +10,8 @@ class MatchController:
             print("Matches:")
             for idx, match in enumerate(self.matches, start=1):
                 print(
-                    f"{idx}. {match.player_1.first_name} vs {match.player_2.first_name} "
-                    f"(Scores: {match.score_1} - {match.score_2})"
+                    f"{idx}. {match.player_1.first_name} vs "
+                    f"{match.player_2.first_name} (Scores: {match.score_1} - "
+                    f"{match.score_2})"
                 )
-
 
